e14aa106c73011ad2f16ac873a8cf1b57c8c1cf6
soyesex <suarezjohan740@gmail.com>
2025-03-07 15:59:59 -0500

    Plantilla ya medianamente estructurada: falta el carrito de compras y el catalogo y ponerlo mas bonito

---
+++ Controllers/CatalogoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aplicacion.Controllers
+{
+    public class CatalogoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
