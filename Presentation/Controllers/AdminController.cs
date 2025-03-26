fbd8831f9589941a6b597cb65ba2c1c07a2becc2
soyesex <suarezjohan740@gmail.com>
2025-03-26 17:34:02 -0500

    Arreglando el login

---
+++ Presentation/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aplicacion.Presentation.Controllers
+{
+    [Authorize(Roles = "Administrador")]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
