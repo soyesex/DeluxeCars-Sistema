e4e65c7dfe2ee278f9a95676d745d341bd3087cb
andrew2325 <andresuarez777a@gmail.com>
2025-07-02 10:09:32 -0500

    Actualización de API Stock y DataAccess

--- DeluxeCarsSistema/DeluxeCars.DataAccess/AppDbContext.cs
+++ DeluxeCarsSistema/DeluxeCars.DataAccess/AppDbContext.cs
@@ -87,7 +87,15 @@ namespace DeluxeCars.DataAccess
                     AdminPINSalt = new byte[] { 200, 83, 160, 16, 78, 248, 207, 38, 92, 77, 89, 102, 153, 114, 81, 115, 83, 30, 186, 233, 102, 213, 73, 2, 96, 217, 113, 195, 170, 196, 242, 251, 129, 207, 218, 130, 213, 87, 159, 92, 195, 57, 218, 61, 141, 228, 57, 69, 141, 252, 79, 232, 234, 248, 158, 153, 105, 6, 107, 229, 59, 2, 96, 151, 68, 31, 241, 195, 1, 90, 56, 145, 138, 20, 210, 114, 191, 199, 75, 61, 21, 160, 101, 184, 223, 64, 29, 126, 194, 161, 64, 192, 154, 154, 89, 83, 176, 121, 121, 121, 206, 132, 229, 52, 47, 239, 88, 93, 102, 226, 96, 15, 61, 150, 137, 82, 38, 54, 163, 54, 136, 220, 154, 111, 209, 217, 250, 29 }
                 }
             );
-
+            modelBuilder.Entity<Categoria>().HasData(
+                new Categoria { Id = 1, Nombre = "Frenos", Descripcion = "Componentes del sistema de frenado." },
+                new Categoria { Id = 2, Nombre = "Suspensión y Dirección", Descripcion = "Amortiguadores, rótulas y componentes de dirección." },
+                new Categoria { Id = 3, Nombre = "Motor", Descripcion = "Partes internas y externas del motor." },
+                new Categoria { Id = 4, Nombre = "Filtros", Descripcion = "Filtros de aceite, aire, combustible y cabina." },
+                new Categoria { Id = 5, Nombre = "Sistema Eléctrico", Descripcion = "Baterías, alternadores y sensores." },
+                new Categoria { Id = 6, Nombre = "Transmisión", Descripcion = "Componentes de la caja de cambios y embrague." },
+                new Categoria { Id = 7, Nombre = "Llantas y Rines", Descripcion = "Neumáticos y rines de varias medidas." }
+            );
             // --- CONFIGURACIÓN DE ENTIDADES ---
 
             // Usuario
